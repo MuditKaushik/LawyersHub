99cc6c093e4f3f9dc59c013f49b838c0f89e5983
Mudit Kaushik <muditm18@gmail.com>
2017-12-03 22:39:02 +0530

    gulpfile.ts modified.

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,18 +5,18 @@ enum paths {
     template_src = './src/proj/templates/**/*',
     images_src = './src/proj/images/**/*',
     template_dest = './app/proj/templates/',
-    images_dest = './app/proj/images/'
-};
+    images_dest = './app/proj/images/',
+}
 
-gulp.task("copy_templates", () => {
+gulp.task('copy_templates', () => {
     return gulp.src(paths.template_src).pipe(gulp.dest(paths.template_dest));
 });
 
-gulp.task("copy_images", () => {
+gulp.task('copy_images', () => {
     return gulp.src(paths.images_src).pipe(gulp.dest(paths.images_dest));
 });
 
-gulp.task("watcher", () => {
+gulp.task('watcher', () => {
     gulp.watch(paths.template_src, () => {
         return gulp.src(paths.template_src).pipe(gulp.dest(paths.template_dest));
     });
@@ -25,10 +25,10 @@ gulp.task("watcher", () => {
     });
 });
 
-gulp.task("build", () => {
-    return run_sequence(["copy_templates", "copy_images"]);
+gulp.task('build', () => {
+    return run_sequence(['copy_templates', 'copy_images']);
 });
 
-gulp.task("default", () => {
-    return run_sequence(["copy_templates", "copy_images", "watcher"]);
+gulp.task('default', () => {
+    return run_sequence(['copy_templates', 'copy_images', 'watcher']);
 });
